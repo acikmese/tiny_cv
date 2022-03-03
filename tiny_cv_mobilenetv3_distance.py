721eeac94075abee60e5b61d452c55406bcde253
acikmese <yasinacikmese@gmail.com>
2022-03-03 10:22:31 +0300

    Add files via upload

--- tiny_cv_mobilenetv3_distance.py
+++ tiny_cv_mobilenetv3_distance.py
@@ -7,6 +7,34 @@ from coco_names import coco_category_names as coco_names
 import time
 import os
 
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
 SAVE_TXT = True
 TXT_PATH = "output.txt"
 SHOW_VID = True
@@ -72,9 +100,13 @@ transform = transforms.Compose([
 ]
 )
 # Set camera.
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap = cv2.VideoCapture(0)
+flip = 2
+cap = cv2.VideoCapture(gstreamer_pipeline(flip_method=flip), cv2.CAP_GSTREAMER)
+# Set camera.
+#cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 # To count total frame processed.
 frame_count = 0
 # To get final frames per second.
